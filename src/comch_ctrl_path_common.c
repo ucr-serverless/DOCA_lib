407cf8058599d9911ce0f6ead0d32771c21fafb7
songyu <localhost>
2025-01-15 00:02:51 -0800

    FIX: comch recv queue sz

--- src/comch_ctrl_path_common.c
+++ src/comch_ctrl_path_common.c
@@ -41,7 +41,7 @@
 #define MAX_MSG_SIZE 65535 /* Max message size */
 DOCA_LOG_REGISTER(COMCH_CTRL_PATH_COMMON);
 
-#define CC_REC_QUEUE_SIZE 100000 /* Maximum amount of message in queue */
+#define CC_REC_QUEUE_SIZE 8192 /* Maximum amount of message in queue */
 #define CC_SEND_TASK_NUM 1024    /* Number of CC send tasks  */
 
 void basic_send_task_completion_callback(struct doca_comch_task_send *task, union doca_data task_user_data,
