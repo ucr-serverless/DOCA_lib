142658ea6cdb6dc87536263d28875ae23337f2d2
songyu <localhost>
2025-01-26 14:15:37 -0800

    CHANGE: max comch send queue length

--- src/comch_ctrl_path_common.c
+++ src/comch_ctrl_path_common.c
@@ -42,7 +42,7 @@
 DOCA_LOG_REGISTER(COMCH_CTRL_PATH_COMMON);
 
 #define CC_REC_QUEUE_SIZE 8192 /* Maximum amount of message in queue */
-#define CC_SEND_TASK_NUM 1024  /* Number of CC send tasks  */
+#define CC_SEND_TASK_NUM 10240  /* Number of CC send tasks  */
 
 void basic_send_task_completion_callback(struct doca_comch_task_send *task, union doca_data task_user_data,
                                          union doca_data ctx_user_data)
